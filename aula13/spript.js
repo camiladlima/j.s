8fb4d28fce53d7753a0e783b9f69deaa4d18f55a
camila <166936999+camiladlima@users.noreply.github.com>
2024-06-13 20:54:11 -0300

    fotos em png e finalização do curso

--- aula13/spript.js
+++ aula13/spript.js
@@ -6,10 +6,16 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora} horas`
     if (hora >= 0 && hora < 12 ){
         // BOM DIA
+        img.src = 'fotos/manha1.png'
+        document.body.style.background = '#fddfb1'
     } else if (hora >= 12 && hora < 18) {
         // BOA TARDE
+        img.src = 'fotos/tarde1.png'
+        document.body.style.background = '#e47d43'
     } else {
         // BOA NOITE
+        img.src = 'fotos/noite1.png'
+        document.body.style.background = '#3d4c5f' 
     }
         
 }
