7348ed53cae3a8a34dc8e7eec9cf2c10e17c2511
camila <166936999+camiladlima@users.noreply.github.com>
2024-06-28 20:47:39 -0300

    aula

---
+++ aula15/ambiente.js
@@ -0,0 +1,9 @@
+var num = [5, 8, 2, 9, 3]
+//num[5] = 10  A posição dele vai ser onde o índice dizer
+num.push(1) // Assim ele vai deduzir que é o último colocado
+ // num.length  Ele é um atributo, o comprimento do vetor, pode mostrar a posição do vetor
+num.sort() // vai pegar todos os valores e colocar em ordem crescente
+
+console.log(num)
+console.log(`O vetor tem ${num.length} Posições`)
+console.log(`O primeiro valor do vetor  ${num[0]}`)
